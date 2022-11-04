0773ff3c272eb8b8a5a7995a27fcd3f4208d65c9
fkrein1 <felipekrein@gmail.com>
2022-11-04 13:57:36 -0300

    feat: remove dist from git

--- backend/dist/routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var findAllCoffeesController_1 = require("./useCases/findAllCoffees/findAllCoffeesController");
-var routes = (0, express_1.Router)();
-var findAllCoffeesController = new findAllCoffeesController_1.FindAllCoffeesController();
-routes.use('/coffees', findAllCoffeesController.handle);
-exports["default"] = routes;
-//# sourceMappingURL=routes.js.map
